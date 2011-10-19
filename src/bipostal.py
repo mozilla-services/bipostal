3ac6fe051b4c3f0547543e5a0ce3106428f299dd
jrconlin <jconlin+git@mozilla.com>
2011-10-19 14:16:11 -0700

    fix to milter, add postmap
    the bipostal milter, sadly, does not do address corrections.
    so bipostmap attempts to.
    
    Yes, there will be test cases and documentation. Later. right now
    I need to get this working.

--- src/bipostal.py
+++ src/bipostal.py
@@ -18,14 +18,19 @@ class ResolveAddress:
         self.db_port = int(self.config.get('redis.port', '6379'))
         self.redis = redis.Redis(host = self.db_host,
                                  port = self.db_port)
+        logger.debug("Redis connection established")
 
     def resolveToken(self, token):
         try:
             # Is there a better email address tokenizer? Do we need one?
             local = token.split('@')[0]
+            logger.debug("Looking up token %s" % local)
             user_address = self.redis.get('t2u:%s' % local)
             if user_address is not None:
+                logger.debug("returning %s" % local)
                 return user_address
+            logger.debug("Not found!")
+            return token
         except Exception, e:
             self.logger("Exception [%s]" % str(e))
             raise
@@ -35,11 +40,12 @@ class BiPostalMilter(ppymilterbase.PpyMilter):
     config = None;
     logger = None;
 
-    def __init__(self, config = None, logger = None):
-        self.config = config
-        self.logger = logger
+    def __init__(self):
+        self.logger = getLogger()
+        self.config = getConfig()
+        logger.info("Initializing BiPostal")
         super(BiPostalMilter, self).__init__()
-        resolver = ResolveAddress(config=config,
+        self.resolver = ResolveAddress(config=config,
                                   logger=logger)
         self.CanAddHeaders()
         self.CanChangeHeaders()
@@ -47,25 +53,47 @@ class BiPostalMilter(ppymilterbase.PpyMilter):
 
     def OnRecptTo(self, cmd, rcpt_to, esmtp_info):
         try:
+            self.logger.debug("OnRecpTo: %s" % rcpt_to)
             address = resolver.resolveToken(rcpt_to)
             self._mutations.append(self.ChangeHeader(0, 'To', address))
         except Exception, e:
-            logger.error("Discarding message: [%s]", str(e))
+            self.logger.error("Discarding message: [%s]", str(e))
             self.Discard()
 
     def OnEndBody(self, cmd):
         actions = self._mutations
+        self.logger.debug("Applying mutations")
         self._mutations = []
         return self.ReturnOnEndBodyActions(actions)
 
     def OnResetState(self):
+        self.logger.info("Resetting")
         self._mutations = []
+
+    def OnMacro(self, cmd, macro_cmd, data):
+        if macro_cmd == 'R':
+
+            address = self.resolver.resolveToken(data[5])
+            if address is not None:
+                local, host = address.split('@',2)
+                data[5] = local
+                data[3] = host
+                return data
+        return None
+
+def getLogger():
+    logging.basicConfig(stream = sys.stderr, level = logging.INFO)
+    return logging.getLogger()
+
+def getConfig(logger = None):
+    if logger is None:
+        logger = getLogger()
+    return Config('bipostal.ini').get_map()
+
 
 if __name__ == '__main__':
     appName = sys.argv[0].split('.')[0]
-    import pdb; pdb.set_trace();
-    logging.basicConfig(stream = sys.stderr, level = logging.INFO)
-    logger = logging.getLogger()
+    logger = getLogger()
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'c:d', ['config='])
         config = None
@@ -82,7 +110,9 @@ if __name__ == '__main__':
     except Exception, e:
         logger.error("Unhandled exception encountered. %s" % str(e))
         exit()
-    bipostal = BiPostalMilter(config=config, logger=logger)
-    ppymilterserver.AsyncPpyMilterServer(config.get('default.port', 9999),
-                                         bipostal)
+    port = config.get('default.port', 9999)
+    import pdb; pdb.set_trace();
+    logger.info("Starting bipostal on port: %s" % port)
+#    bipostal = BiPostalMilter(config=config, logger=logger)
+    ppymilterserver.AsyncPpyMilterServer(port, BiPostalMilter)
     asyncore.loop()
---
+++ src/bipostmap.py
@@ -0,0 +1,161 @@
+import asyncore
+import asynchat
+import getopt
+import redis
+import os
+import sys
+import logging
+import socket
+from config import Config
+
+class ResolveAddress(object):
+    def __init__(self, config = None):
+        self.config = config
+        self.db_host = self.config.get('redis.host', 'localhost')
+        self.db_port = int(self.config.get('redis.port', '6379'))
+        try:
+            self.redis = redis.Redis(host = self.db_host,
+                                 port = self.db_port)
+            logging.debug("Redis connection established")
+        except Exception, e:
+            logging.error("Redis connection failed %s",  str(e))
+            raise BiPostmapException("DB Failure")
+
+    def resolveToken(self, token):
+        try:
+            local = token.split('@')[0]
+            user_address = self.redis.get('t2u:%s' % local)
+            if user_address is not None:
+                logging.debug('returning %s' % local)
+                return user_address
+            logging.debug("Not found!")
+            return None
+        except Exception, e:
+            logging.error("Unhandled Exception [%s]" % str(e))
+            raise
+
+
+class BiPostmapException(Exception):
+    """ Browser ID Postmap Exception """
+    def __init__(self, code, message):
+        self.code = code
+        self.msg = message
+
+    def __str__(self):
+        return "[%s] %s" % (self.code, self.msg)
+
+
+class BiPostmapServer(asyncore.dispatcher):
+
+    def __init__(self, port, server_class, config=None):
+        asyncore.dispatcher.__init__(self)
+        self._server_class = server_class
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(('', config.get('postmap.port', 9998)))
+        self.listen(config.get('postmap.max_queued_connections', 1024))
+        self.config = config
+
+    def handle_accept(self):
+        """Callback function from asyncore to handle a connection
+        dispatching."""
+        try:
+            (conn, addr) = self.accept()
+        except socket.error, e:
+            logging.warn('warning: server accept() threw an exception ("%s")',
+                          str(e))
+            return None
+        BiPostmapConnectionHandler(conn,
+                                    addr,
+                                    self._server_class,
+                                    config)
+
+
+class BiPostmapDispatcher(object):
+
+    def __init__(self, server_class, config = None):
+        self._server_class = server_class(config)
+
+    def dispatch(self, data):
+        try:
+            data = data.strip().split(' ')[1]
+            username = self._server_class.resolveToken(data)
+            if username:
+                return {'name': username, 'code': 200}
+            return {'name': 'Unrecognized Address', 'code': 500}
+        except Exception, e:
+            import pdb; pdb.set_trace();
+            logging.info('Perm Failure: %s', str(e))
+            return {'name': 'Lookup failure', 'code': 400}
+
+
+class BiPostmapConnectionHandler(asynchat.async_chat):
+
+    def __init__(self, conn, addr, server_class, config=None):
+        asynchat.async_chat.__init__(self, conn)
+        self._conn = conn
+        self._addr = addr
+        self._dispatcher = BiPostmapDispatcher(server_class, config)
+        self._input = []
+        self.set_terminator("\n")
+        self.found_terminator = self.read_data
+
+    def collect_incoming_data(self, data):
+        """Callback from asynchat--simply buffer partial data in a string."""
+        self._input.append(data)
+
+    def log_info(self, message, type='info'):
+        """Provide useful logging for uncaught exceptions"""
+        if type == 'info':
+            logging.debug(message)
+        else:
+            logging.error(message)
+
+    def respond(self, response):
+        if 'name' not in response:
+            raise BiPostmapException("missing return name")
+        logging.debug('  >>> [%s] %s' % (response.get('code', 400),
+                                         response.get('name')))
+        self.push("%d %s \n" % (int(response.get('code', 400)),
+                                str(response.get('name'))))
+
+    def read_data(self):
+        inbuff = "".join(self._input)
+        self._input = []
+        logging.debug(' request: %s' % inbuff)
+        try:
+            response = self._dispatcher.dispatch(inbuff)
+            if type(response) == list:
+                for r in response:
+                    self.respond(r)
+            elif response:
+                self.respond(response)
+            self.close()
+        except Exception, e:
+            import pdb; pdb.set_trace()
+            self.respond({'name': 'Unexpected error', 'code': 400})
+
+
+if __name__ == '__main__':
+    appName = sys.argv[0].split('.')[0]
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'c:d', ['config='])
+        config = None
+        for opt, val in opts:
+            if opt in '--config':
+                config = Config(val).get_map()
+        if config is None:
+            iniFileName = '%s.ini' % appName
+            if os.path.exists(iniFileName):
+                config = Config(iniFileName).get_map()
+        if config is None:
+            logging.error("No configuration found. Aborting")
+            exit()
+    except Exception, e:
+        logging.error("Unhandled exception encountered. %s" % str(e))
+        exit()
+    port = config.get('default.port', 9999)
+    logging.info("Starting bipostmap on port: %s" % port)
+    # Sets the BiPostmapServer as the async core server.
+    server = BiPostmapServer(port, ResolveAddress, config = config)
+    asyncore.loop()
