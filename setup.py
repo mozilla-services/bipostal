b20e1df346289b8fed7914acb48f412d0814fa19
jrconlin <jconlin+git@mozilla.com>
2011-11-01 14:40:45 -0700

    Adding missing template, setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'Redis',
+    'Distribute',
+    'Mako'
+]
+entry_points = """
+"""
+
+try:
+    with open('README.txt') as file:
+        long_desc = file.read()
+except:
+    long_desc = "TBD"
+
+setup(name="BIPostal",
+      author="Mozilla Services Group",
+      description="Postfix/Sendmail Milter for BrowserId address tokens",
+      long_description=long_desc,
+      author_email="dev_services@mozilla.com",
+      version=0.1,
+      packages=find_packages(),
+      entry_points=entry_points,
+      install_requires=requires,
+      license='MPL')
